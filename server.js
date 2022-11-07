40aa9a99a81d65912772f3ca8b4c534259d3804a
Benjamin Hites <bjhites@email.unc.edu>
2022-11-07 18:02:12 -0500

    adding pack sides,dice, rolls

--- server.js
+++ server.js
@@ -23,17 +23,17 @@ app.get('/app/roll', (req, res, next) => {
 //	res.status(200).send(JSON.stringify(roll.roll(req.body.sides, req.body.dice, req.body.rolls)));
  //})
 
-// app.get('/app/roll/:sides', (req, res, next) => {
-// 	res.status(200).send(JSON.stringify(roll(req.params['sides'], 2, 1)));
-// })
+app.get('/app/roll/:sides', (req, res, next) => {
+	res.status(200).send(JSON.stringify(roll(req.params['sides'], 2, 1)));
+})
 
-// app.get('/app/roll/:sides/:dice', (req, res, next) => {
-//	res.status(200).send(JSON.stringify(roll(req.params['sides'], req.params['dice'], 1)));
-// })
+app.get('/app/roll/:sides/:dice', (req, res, next) => {
+	res.status(200).send(JSON.stringify(roll(req.params['sides'], req.params['dice'], 1)));
+})
 
-// app.get('/app/roll/:sides/:dice/:rolls', (req, res, next) => {
-//	res.status(200).send(JSON.stringify(roll(req.params['sides'], req.params['dice'], req.params['rolls'])));
-// })
+app.get('/app/roll/:sides/:dice/:rolls', (req, res, next) => {
+	res.status(200).send(JSON.stringify(roll(req.params['sides'], req.params['dice'], req.params['rolls'])));
+})
 
 app.get('*', function(req, res){
 	res.status(404).send("404 NOT FOUND");
