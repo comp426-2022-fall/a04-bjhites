c013d59f967cc67b9e6fa2317819736d8d145ed0
Benjamin Hites <bjhites@email.unc.edu>
2022-11-07 18:12:20 -0500

    Added content type, post request

--- server.js
+++ server.js
@@ -16,22 +16,26 @@ app.get('/app', (req, res, next) => {
 })
 
 app.get('/app/roll', (req, res, next) => {
+ 	res.setHeader("Content-Type", "application/json"); 	
 	res.status(200).send(JSON.stringify(roll(6, 2, 1)));
 })
 
-// app.post('/app/roll', (req, res, next) => {
-//	res.status(200).send(JSON.stringify(roll.roll(req.body.sides, req.body.dice, req.body.rolls)));
- //})
+ app.post('/app/roll', (req, res, next) => {
+	res.status(200).send(JSON.stringify(roll.roll(req.body.sides, req.body.dice, req.body.rolls)));
+ })
 
 app.get('/app/roll/:sides', (req, res, next) => {
+ 	res.setHeader("Content-Type", "application/json"); 	
 	res.status(200).send(JSON.stringify(roll(req.params['sides'], 2, 1)));
 })
 
 app.get('/app/roll/:sides/:dice', (req, res, next) => {
+ 	res.setHeader("Content-Type", "application/json"); 	
 	res.status(200).send(JSON.stringify(roll(req.params['sides'], req.params['dice'], 1)));
 })
 
 app.get('/app/roll/:sides/:dice/:rolls', (req, res, next) => {
+ 	res.setHeader("Content-Type", "application/json"); 	
 	res.status(200).send(JSON.stringify(roll(req.params['sides'], req.params['dice'], req.params['rolls'])));
 })
 
@@ -40,5 +44,6 @@ app.get('*', function(req, res){
 })
 
 app.listen(port, () => {
+	console.log(port);
 })
 
